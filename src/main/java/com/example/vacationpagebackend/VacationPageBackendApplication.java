c5f2804001ed94f0bd67abe3316ab8fc3e8b7adb
alweb29 <albertow55@gmail.com>
2024-04-14 19:21:31 +0200

    initial project

---
+++ src/main/java/com/example/vacationpagebackend/VacationPageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacationpagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationPageBackendApplication.class, args);
+    }
+
+}
