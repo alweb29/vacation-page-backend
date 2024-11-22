b890c6731ebbc72c75131aa926173975a3c9984d
alweb29 <albertow55@gmail.com>
2024-11-22 20:08:39 +0100

    Updated logs

--- src/main/java/com/example/vacationpagebackend/controller/RoomRecordController.java
+++ src/main/java/com/example/vacationpagebackend/controller/RoomRecordController.java
@@ -6,6 +6,7 @@ import com.example.vacationpagebackend.entity.RoomType;
 import com.example.vacationpagebackend.service.EmailSenderService;
 import com.example.vacationpagebackend.service.RoomRecordService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/room-records")
 @RequiredArgsConstructor
+@Slf4j
 public class RoomRecordController {
 
     private final RoomRecordService roomRecordService;
@@ -23,6 +25,7 @@ public class RoomRecordController {
     @GetMapping
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<List<RoomRecord>> getAllRoomRecords() {
+        log.info("Get all room records");
         List<RoomRecord> roomRecords = roomRecordService.getAllRoomRecords();
         return ResponseEntity.ok(roomRecords);
     }
@@ -30,6 +33,7 @@ public class RoomRecordController {
     @GetMapping("/{monthNum}/{roomType}")
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<RoomRecordWithDaysWritten> getRoomRecord(@PathVariable int monthNum, @PathVariable int roomType) {
+        log.info("Get room record for month number " + monthNum + " and room type " + roomType);
         RoomType type = getRoomType(roomType);
         RoomRecordWithDaysWritten response = roomRecordService.getRoomRecordsForMonthAndType(monthNum,type);
         return ResponseEntity.ok(response);
@@ -39,6 +43,7 @@ public class RoomRecordController {
     @PostMapping
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<RoomRecord> createRoomRecord(@RequestBody RoomRecord roomRecord) {
+        log.info("Creating a new room record");
         RoomRecord createdRoomRecord = roomRecordService.createRoomRecord(roomRecord);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdRoomRecord);
     }
@@ -46,7 +51,7 @@ public class RoomRecordController {
     @PostMapping("/reservation")
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity sendEmailToAdmin(@RequestBody Mail mail){
-        System.out.println("Sending Reservation Form! \n" + mail.toString());
+        log.info("Sending Reservation Form! \n" + mail.toString());
         if(emailSenderService.sendEmail(mail)){
             return ResponseEntity.ok().build();
         }else {
@@ -57,6 +62,7 @@ public class RoomRecordController {
     @PutMapping("/{id}")
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<RoomRecord> updateRoomRecord(@PathVariable Long id, @RequestBody RoomRecord roomRecord) {
+        log.info("Updating room record with id " + id);
         RoomRecord updatedRoomRecord = roomRecordService.updateRoomRecord(id, roomRecord);
         return ResponseEntity.ok(updatedRoomRecord);
     }
@@ -64,6 +70,7 @@ public class RoomRecordController {
     @DeleteMapping("/{id}")
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<Void> deleteRoomRecord(@PathVariable Long id) {
+        log.info("Deleting room record with id " + id);
         roomRecordService.deleteRoomRecord(id);
         return ResponseEntity.noContent().build();
     }
@@ -71,6 +78,7 @@ public class RoomRecordController {
     @DeleteMapping("/{monthNum}/{roomType}")
     @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS })
     public ResponseEntity<RoomRecord> deleteRoomRecord(@PathVariable int monthNum, @PathVariable int roomType) {
+        log.info("Deleting room record with id " + roomType);
         RoomType type = getRoomType(roomType);
         List<RoomRecord> records = roomRecordService.getRoomRecordsAsListByMonthAndType(monthNum,type);
         for (RoomRecord roomRecord : records){
